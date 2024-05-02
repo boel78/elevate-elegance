ea70dc225f2ebf8f2d40255dc6eb93bf7741e556
boel78 <95147718+boel78@users.noreply.github.com>
2024-05-02 09:12:03 +0200

    skapade frontend

---
+++ frontend/elevate/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "../components/navbar";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
