c58fd6887773b733afee7b4f9a0d6d1d51cc87b3
boel78 <95147718+boel78@users.noreply.github.com>
2024-05-24 10:22:11 +0200

    Skapat test och börjat med rest api

--- backend/ecommerce-backend/src/main/java/com/elevate/ecommercebackend/EcommerceBackendApplication.java
+++ backend/ecommerce-backend/src/main/java/com/elevate/ecommercebackend/EcommerceBackendApplication.java
@@ -1,13 +1,27 @@
 package com.elevate.ecommercebackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 	}
+
+	@Autowired
+	TestObjectRepository repository;
+
+	void createTestObjects() {
+		repository.save(new TestObject("1", "Bajs"));
+		repository.save(new TestObject("2", "Najs"));
+	}
+
+
+
 
 }
