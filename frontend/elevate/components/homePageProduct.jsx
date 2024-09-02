bca5e20b6e19c7b8d66bf1443cc81959254e4426
boel78 <95147718+boel78@users.noreply.github.com>
2024-09-02 12:46:16 +0200

    la till funktionalitet på homepageproduct
    la till add to cart fiunktionalitet

--- frontend/elevate/components/homePageProduct.jsx
+++ frontend/elevate/components/homePageProduct.jsx
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom';
+import { MenuContext } from '../src/menuContext';
 
 export const HomePageProduct = (props) => {
     const [img, setImg] = useState()
+    const {cart, setCart} = useContext(MenuContext)
     
     useEffect(() => {
-      console.log(props)
       setImg(props.data.image)
     }, [])
+
+    const addToCart = () => {
+      const product = props
+      
+      setCart([...cart, product])
+      console.log(cart)
+    }
 
   return (
     <div className="w-1/5 h-auto rounded-xl border-solid border-2 border-black">
@@ -22,7 +30,7 @@ export const HomePageProduct = (props) => {
                 <Link to={`/product/${props.data.id}`}>              
                   <button className='text-white bg-blue-950'>To Product</button>
                 </Link>
-                <button>Add to cart</button>
+                <button onClick={addToCart}>Add to cart</button>
             </div>
         </div>
     </div>
