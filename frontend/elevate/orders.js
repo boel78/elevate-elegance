b1582778f6bbeb8820e9da94c5986a79142d0d3f
boel78 <95147718+boel78@users.noreply.github.com>
2024-09-05 14:34:14 +0200

    dynamiska ordrar
    man kan lägga till hur många ordrar på ordersidan som helst

--- frontend/elevate/orders.js
+++ frontend/elevate/orders.js
@@ -5,6 +5,15 @@ export const ORDERS = [
             1,
             2,
         ],
-        u_id: 1
+        u_id: 1,
+        eta: "2025-04-02"
+    },
+    {
+        id: 2,
+        p_id: [
+            1
+        ],
+        u_id: 1,
+        eta: "2025-08-02"
     }
 ]
--- frontend/elevate/pages/Orders/myOrders.jsx
+++ frontend/elevate/pages/Orders/myOrders.jsx
@@ -12,32 +12,32 @@ export const MyOrders = () => {
     setUserOrders(orders);
   }, [currentUser]);
 
-
   return (
     <div>
       <div>
-        <div>
-          <div className="pt-20">
-            {userOrders.map((order) => (
-                order.p_id.map((productID) => {
-                  const product = PRODUCTS.find(
-                    (prod) => prod.id === productID
-                  );
-                  return (
-                    product && (
-                      <div key={product.id}>
-                        <img src={product.image} />
-                      </div>
-                    )
-                  );
-                })
-            ))}
+        {userOrders.map((order) => (
+          <div
+            className="flex pt-20 justify-between border-solid border-2 border-black ml-40 mr-40"
+            key={order.id}
+          >
+            <div className="flex gap-4">
+              {order.p_id.map((productID) => {
+                const product = PRODUCTS.find((prod) => prod.id === productID);
+                return (
+                  product && (
+                    <div key={product.id}>
+                      <img src={product.image} className="max-w-20" />
+                    </div>
+                  )
+                );
+              })}
+            </div>
+            <div className="flex flex-col">
+              <button className="bg-slate-600">Track order</button>
+              <button>View Order Details</button>
+            </div>
           </div>
-        </div>
-        <div>
-          <button>Track order</button>
-          <button>View Order Details</button>
-        </div>
+        ))}
       </div>
     </div>
   );
