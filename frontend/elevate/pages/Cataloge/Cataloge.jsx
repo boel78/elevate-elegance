b037b5d7025546a179e39df1e79404c0c4dc80a8
boel78 <95147718+boel78@users.noreply.github.com>
2024-09-05 11:54:14 +0200

    Började med en dropdown i cataloge category

--- frontend/elevate/pages/Cataloge/Cataloge.jsx
+++ frontend/elevate/pages/Cataloge/Cataloge.jsx
@@ -6,14 +6,17 @@ import Axios from "axios";
 import { HomePageProduct } from "../../components/homePageProduct";
 import { Sidemenu } from "../../components/sidemenu";
 import { PRODUCTS } from "../../products";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { ArrowDown } from "@phosphor-icons/react";
 
 export const Cataloge = () => {
   const { Category } = useParams();
 
-  const { filteredProducts, sideMenuActive} =
-    useContext(MenuContext);
+  const { filteredProducts, sideMenuActive } = useContext(MenuContext);
   const [products, setProducts] = useState([]);
+  const [showCategory, setShowCategory] = useState(false);
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     /*Axios.get("http://localhost:8080/api/product").then((res) => {
@@ -28,18 +31,58 @@ export const Cataloge = () => {
       setProducts(newProducts);
     }
     console.log(Category);
-  }, []);
+  }, [Category]);
+
+  const handleSetShowCategory = () => {
+    setShowCategory(!showCategory);
+  };
+
+  const handleSetCategory = (category) => {
+    navigate(`/cataloge/${category}`)
+    setShowCategory(false)
+  }
 
   return (
     <>
       <Navbar />
       <div className="bg-lightTan flex flex-col gap-20">
         <div className="flex justify-end">
-          <div className="flex pt-20">
-            <p>Category</p>
-            <p>Filters</p>
-            <p>Sort by</p>
+          <div className="flex pt-20 flex-col relative">
+              <div className="flex"> 
+                <span
+                  onClick={handleSetShowCategory}
+                  className="flex items-center justify-center"
+                >
+                  <p>Category</p>
+                  <ArrowDown size="15" />
+                </span>
+              <span className="flex items-center justify-center">
+                <p>Filters</p>
+                <ArrowDown size="15" />
+              </span>
+              <span className="flex items-center justify-center">
+                <p>Sort by</p>
+                <ArrowDown size="15" />
+              </span>
+              </div>
+              { showCategory &&
+              <ul className='absolute top-full left-0 mt-2 z-10 bg-slate-100'>
+                  <li onClick={() => handleSetCategory("Clothing")}>
+                    Clothing
+                  </li>
+                  <li>
+                    Jewellery
+                  </li>
+                  <li>
+                    Bags
+                  </li>
+                  <li>
+                    Shoes
+                  </li>
+              </ul>
+}
           </div>
+
         </div>
         <div>
           {products.map((element) => (
