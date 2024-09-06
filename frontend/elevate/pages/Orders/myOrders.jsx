d26ec4a589624e091f0ba46b5dd3bee69b8c7b00
boel78 <95147718+boel78@users.noreply.github.com>
2024-09-06 09:07:45 +0200

    fixade orders utan login

--- frontend/elevate/pages/Orders/myOrders.jsx
+++ frontend/elevate/pages/Orders/myOrders.jsx
@@ -8,41 +8,51 @@ export const MyOrders = () => {
   const [userOrders, setUserOrders] = useState([]);
 
   useEffect(() => {
-    const orders = ORDERS.filter((order) => currentUser.id == order.u_id);
-    setUserOrders(orders);
+    if (currentUser != null) {
+      const orders = ORDERS.filter((order) => currentUser.id == order.u_id);
+      setUserOrders(orders);
+    }
   }, [currentUser]);
 
   useEffect(() => {
-    noMenus()
-  },[])
+    noMenus();
+  }, []);
 
   return (
     <div>
       <div>
-        {userOrders.map((order) => (
-          <div
-            className="flex pt-20 justify-between border-solid border-2 border-black ml-40 mr-40"
-            key={order.id}
-          >
-            <div className="flex gap-4">
-              {order.p_id.map((productID) => {
-                const product = PRODUCTS.find((prod) => prod.id === productID);
-                return (
-                  product && (
-                    <div key={product.id}>
-                      <img src={product.image} className="max-w-20" />
-                    </div>
-                  )
-                );
-              })}
-              <p>Arriving: {order.eta}</p>
+        {currentUser != null ? (
+          userOrders.map((order) => (
+            <div
+              className="flex pt-20 justify-between border-solid border-2 border-black ml-40 mr-40"
+              key={order.id}
+            >
+              <div className="flex gap-4">
+                {order.p_id.map((productID) => {
+                  const product = PRODUCTS.find(
+                    (prod) => prod.id === productID
+                  );
+                  return (
+                    product && (
+                      <div key={product.id}>
+                        <img src={product.image} className="max-w-20" />
+                      </div>
+                    )
+                  );
+                })}
+                <p>Arriving: {order.eta}</p>
+              </div>
+              <div className="flex flex-col">
+                <button className="bg-slate-600">Track order</button>
+                <button>View Order Details</button>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <button className="bg-slate-600">Track order</button>
-              <button>View Order Details</button>
-            </div>
+          ))
+        ) : (
+          <div className="">
+            <h2 className="pt-20">Please login</h2>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
