7e151661dd2db6bf3fab6bcf57cd1be8bceb8a6a
boel78 <95147718+boel78@users.noreply.github.com>
2024-09-20 12:56:10 +0200

    gjort lite rent

--- frontend/elevate/components/cart.jsx
+++ frontend/elevate/components/cart.jsx
@@ -1,14 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { MenuContext } from "../src/menuContext";
 import { TanButton } from "./button";
-import { ArrowDown } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 import { CartObject } from "./cartObject";
 
 export const Cart = () => {
-  const { cart, setCart, currentUser } = useContext(MenuContext);
+  const { cart, currentUser } = useContext(MenuContext);
   const [totalCost, setTotalCost] = useState(0);
-  const [sizeBarOpen, setSizeBarOpen] = useState(false);
 
   useEffect(() => {
     let total = 0;
@@ -22,42 +20,6 @@ export const Cart = () => {
     });
     setTotalCost(total);
   }, [cart]);
-
-  const handleSizeBarToggle = () => {
-    setSizeBarOpen(!sizeBarOpen);
-  };
-
-  const handleSetSize = (item, s) => {
-    const updatedCart = cart.map((p) => {
-      if (p.product.id === item.product.id) {
-        return { ...p, size: s };
-      }
-      return p;
-    });
-    setCart(updatedCart);
-    setSizeBarOpen(false);
-  };
-
-  const handleChangeQuantity = (val, item) => {
-   if(Number(val) === 0 ){
-    const updatedCart = cart.filter((product) => !(product.id === item.id))
-    setCart(updatedCart)
-   }
-   else{
-    const updatedCart = cart.map((product) => {
-      if (product.id === item.id) {
-          let index = updatedCart.indexOf(product);
-          updatedCart.splice(index, 1);
-          return { ...product, quantity: val };
-        
-      }
-      return product;
-    });
-    
-    setCart(updatedCart);
-  }
-    console.log(cart)
-  };
 
 
   return (
