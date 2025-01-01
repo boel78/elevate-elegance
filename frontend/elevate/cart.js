bbbf8ea3573d9505f1b098be6e7d869058a37e43
boel78 <95147718+boel78@users.noreply.github.com>
2025-01-01 11:49:28 +0100

    tog bort lokal exempeldata

--- frontend/elevate/cart.js
+++
@@ -1,8 +0,0 @@
-export const CARTPRODUCTS = [
-    {
-        p_id: 1,
-        u_id: 1,
-        size: "s",
-        quantity: 1
-    }
-]
--- frontend/elevate/savedItems.js
+++
@@ -1,9 +0,0 @@
-export const SAVEDITEMS = [
-    {
-        u_id: 1,
-        p_id: [
-            1,
-            2  
-        ]
-    }
-] 
